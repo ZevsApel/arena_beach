01d0bf5f9262d850dbd72383aefea2e52c8ccaef
Owl0_0 <prechencka473@gmail.com>
2025-09-09 18:45:02 +0300

    fix: fixed GET method

--- app/api/rooms/[id]/route.ts
+++ app/api/rooms/[id]/route.ts
@@ -1,62 +1,69 @@
-import { NextResponse } from "next/server";
-import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
+// ================== GET ==================
 export async function GET(
-    req: Request,
-    { params }: { params: {id: string }}
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    const room = await prisma.room.findUnique({
-        where: { id: Number(params.id ) },
-        include: { features: true, images: true}
-    })
+  const room = await prisma.room.findUnique({
+    where: { id: Number(params.id) },
+    include: { features: true, images: true },
+  });
 
-    return NextResponse.json(room);
+  if (!room) {
+    return NextResponse.json({ error: "Комната не найдена" }, { status: 404 });
+  }
+
+  return NextResponse.json(room);
 }
 
-export async function PUT (
-    req: Request,
-    { params }: { params: { id: string } }
+// ================== PUT ==================
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    const { title, slug, summary, description, price, features, images} = await req.json();
+  const { title, slug, summary, description, price, features, images } =
+    await req.json();
 
-    const updated = await prisma.room.update({
-        where: { id: Number(params.id) },
-        data: {
-            title,
-            slug,
-            summary,
-            description,
-            price,
+  const updated = await prisma.room.update({
+    where: { id: Number(params.id) },
+    data: {
+      title,
+      slug,
+      summary,
+      description,
+      price,
 
-            features: {
-                deleteMany: {},
-                create: features.map((f: { label: string, icon: string}) => ({
-                    label: f.label,
-                    icon: f.icon
-                })),
-            },
+      features: {
+        deleteMany: {},
+        create: features.map((f: { label: string; icon: string }) => ({
+          label: f.label,
+          icon: f.icon,
+        })),
+      },
 
+      images: {
+        deleteMany: {},
+        create: images.map((i: { path: string }) => ({
+          path: i.path,
+        })),
+      },
+    },
+    include: { features: true, images: true },
+  });
 
-            images: {
-                deleteMany: {},
-                create: images.map((i: { path: string }) => ({
-                    path: i.path
-                })),
-            },
-        },
-        include: { features: true, images: true}
-    });
-
-    return NextResponse.json({updated, message: 'Обновлено'});
+  return NextResponse.json({ updated, message: "Обновлено" });
 }
 
+// ================== DELETE ==================
 export async function DELETE(
-    req: Request,
-    { params }: { params: { id: string} }
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-    await prisma.room.delete({
-        where: { id: Number(params.id) }
-    });
+  await prisma.room.delete({
+    where: { id: Number(params.id) },
+  });
 
-    return NextResponse.json({message: 'Комната удалена' });
+  return NextResponse.json({ message: "Комната удалена" });
 }
